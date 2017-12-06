23b582c2ab4127d22f837c9f0ca35ce3719d1b1c
SprinkIV <a.chiron444@hotmail.fr>
2017-12-06 14:05:51 +0100

    displayHelp

--- Cours1/Program.cs
+++ Cours1/Program.cs
@@ -8,9 +8,31 @@ namespace Cours1
 {
     class Program
     {
+
+        public const string ARGS_DIRECTORY = "-d";
+
+        // Arguments
+        // -d <Dossier à vérifier> : Précise le dossier à vérifier pour l'import
+
         static void Main(string[] args)
         {
+            if (args[0] == ARGS_DIRECTORY && args.Length == 2)
+            {
 
+            }
+            else
+            {
+                displayHelp();
+            }
+        }
+
+        /// <summary>
+        /// Affiche la documentation du <see cref="Program"/>
+        /// </summary>
+        private static void displayHelp()
+        {
+            Console.WriteLine("Arguments :");
+            Console.WriteLine("-d <Dossier à vérifier> : Précise le dossier à vérifier pour l'import");
         }
 
         private static void ImportXMLFile(string filePath)
